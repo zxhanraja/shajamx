0bc161540525211d6af9cda8ff21cf75488d4689
Zeeshan Raja <zeeshan89819@gmail.com>
2026-03-28 18:54:02 +0530

    done again

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'three';
+            if (id.includes('gsap')) return 'gsap';
+            if (id.includes('react')) return 'react-vendor';
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 });

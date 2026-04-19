3292c74d0a316dabed2997fb748ebf643235ca53
Zeeshan Raja <zeeshan89819@gmail.com>
2026-04-20 00:27:33 +0530

    fix(build): remove relative base path to prevent client side router blank screen crash

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  base: './',
   plugins: [
     react(),
     compression({ algorithm: 'gzip' })
@@ -14,18 +13,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('three')) {
-              return 'three';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    },
     chunkSizeWarningLimit: 1500,
     assetsInlineLimit: 8192,
     minify: 'esbuild',

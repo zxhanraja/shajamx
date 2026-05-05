197e538a0516b25ef2f7a000f2412e3b825e57c7
Zeeshan Raja <zeeshan89819@gmail.com>
2026-05-06 01:26:20 +0530

    Fix manualchunks build error for vercel

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          three: ['three']
+        manualChunks(id) {
+          if (id.includes('node_modules/three')) {
+            return 'three';
+          }
         }
       }
     },
